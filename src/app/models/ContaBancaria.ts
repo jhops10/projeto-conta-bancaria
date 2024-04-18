e7b20fb824e01a6ca87bb31466d5ef8c10626294
João Paulo <joaowais@gmail.com>
2024-04-18 20:03:02 -0300

    Criando classe ContaBancaria

---
+++ src/app/models/ContaBancaria.ts
@@ -0,0 +1,13 @@
+export abstract class ContaBancaria {
+  constructor(public titular: string, protected saldo: number = 0) {}
+
+  depositar(valor: number): void {
+    this.saldo += valor;
+  }
+
+  abstract sacar(valor: number): void;
+
+  get getSaldo(): number {
+    return this.saldo;
+  }
+}
