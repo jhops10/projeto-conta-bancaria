9fc9d8220e73452f2c8157a5280c2a85f48c86b9
João Paulo <joaowais@gmail.com>
2024-04-19 18:22:57 -0300

    Ligando os dados do template com o TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { ContaBancaria } from './models/ContaBancaria';
+import { ContaService } from './services/conta.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'projeto-conta-bancaria';
+  titular: string = '';
+  tipoConta: string = '';
+  valorTransacao: number | null = null;
+  conta: ContaBancaria | null = null;
+
+  constructor(private contaService: ContaService) {}
+
+  criarConta(): void {
+    if (this.titular != '' && this.tipoConta) {
+      switch (this.tipoConta) {
+        case 'corrente':
+          this.conta = this.contaService.criarContaCorrente(this.titular);
+          console.log(this.conta);
+          break;
+
+        case 'salario':
+          this.conta = this.contaService.criarContaSalario(this.titular);
+          console.log(this.conta);
+          break;
+
+        case 'poupanca':
+          this.conta = this.contaService.criarContaPoupanca(this.titular);
+          console.log(this.conta);
+          break;
+
+        default:
+          console.error('Tipo de conta inválido');
+      }
+    }
+  }
 }
