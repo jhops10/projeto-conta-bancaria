aabfaa568f28774643c268bf6e098461faf70997
João Paulo <joaowais@gmail.com>
2024-04-19 19:01:58 -0300

    Projeto Finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,26 +16,42 @@ export class AppComponent {
   constructor(private contaService: ContaService) {}
 
   criarConta(): void {
-    if (this.titular != '' && this.tipoConta) {
+    if (this.titular === '' || this.tipoConta === '') {
+      alert('Você deve preencher os campos Nome e Tipo de Conta');
+    } else if (this.titular != '' && this.tipoConta) {
       switch (this.tipoConta) {
         case 'corrente':
           this.conta = this.contaService.criarContaCorrente(this.titular);
-          console.log(this.conta);
+
           break;
 
         case 'salario':
           this.conta = this.contaService.criarContaSalario(this.titular);
-          console.log(this.conta);
+
           break;
 
         case 'poupanca':
           this.conta = this.contaService.criarContaPoupanca(this.titular);
-          console.log(this.conta);
+
           break;
 
         default:
           console.error('Tipo de conta inválido');
       }
+    }
+  }
+
+  depositar(): void {
+    if (this.valorTransacao && this.valorTransacao > 0) {
+      this.conta && this.conta.depositar(this.valorTransacao);
+      this.valorTransacao = null;
+    }
+  }
+
+  sacar(): void {
+    if (this.valorTransacao && this.valorTransacao > 0) {
+      this.conta && this.conta.sacar(this.valorTransacao);
+      this.valorTransacao = null;
     }
   }
 }
